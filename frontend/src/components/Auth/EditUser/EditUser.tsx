2118155ee5e7d6828f070e2a7044d6a9e596b2eb
Mikhail3379 <78318602+Mikhail3379@users.noreply.github.com>
2022-02-15 17:17:09 +0000

    Update EditUser.tsx

--- frontend/src/components/Auth/EditUser/EditUser.tsx
+++ frontend/src/components/Auth/EditUser/EditUser.tsx
@@ -1,27 +1,19 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import Users from "../../Users/Users";
 import { getUserByID, updateUser } from "../../../lib/endpoints";
 import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 
-interface IState {
- 
-  userId: string;
-  userData: any;
-}
 
-export default class editUser extends React.Component<any, IState> {
-  static id: string | number | readonly string[] | undefined;
-  static public_key: string | number | readonly string[] | undefined;
-  constructor(props: any) {
-    super(props);
-    this.state = { userId: "", userData: {} };
-  }
-  
 
-  handleUpdateUser = () => {
-    const { id, public_key } = this.state.userData;
+export default function EditUser() {
+  const { id: userId } = useParams();
+  const [userData, setUserData] = useState<any>({});
+  const navigate=useNavigate()
+    const handleUpdateUser = () => {
+    const { id, public_key } = userData;
     axios
       .put(`${updateUser}?id=${id}`, {
         public_key,
@@ -32,59 +24,52 @@ export default class editUser extends React.Component<any, IState> {
           title: "User has been edited successfully",
           showConfirmButton: false,
           timer: 1500,
-        });  
+        });
         // handle success
         console.log({ Users: response.data });
+        navigate("/Users")
+        // window.location.assign("#/Users");
         return response.data;
-        
       });
   };
-  componentDidMount() {
-    const userId = this.props.match.params.id;
-    this.setState({ userId });
-    axios.get(`${getUserByID}?id=${userId}`).then((response) => {
-      this.setState({ userData: response.data[0] });
-    });
-  }
-  handlePublicKeyChange = (event: any) => {
-    const value = event.target.value;
-    const user = { ...this.state.userData };
-    user.public_key = value;
-    this.setState({ userData: user });
-  };
-  public render() {
+  useEffect(() => {
     
-    const { userData } = this.state;
-    return (
+    axios.get(`${getUserByID}?id=${userId}`).then((response) => {
+      setUserData(response.data[0]);
+    });
+  }, []);
+
+  const handlePublicKeyChange = (event: any) => {
+    const value = event.target.value;
+    const user = { ...userData };
+    user.public_key = value;
+    setUserData(user);
+  };
+
+  return (
+    <div className="container">
+      <br />
+      <label className="label">Edit User's public key</label>
+      <br />
+      <input
+        value={userData?.public_key}
+        // onChange={(e) => setPublic_key(e.target.value)}
+
+        type="text"
+        className="input"
+        onChange={handlePublicKeyChange}
+      />
+
+      <br />
+      <button
+        type="submit"
+        className="btn "
+        //@ts-ignore
+        onClick={handleUpdateUser}
+      >
+        Submit
+      </button>
       
-      <div className="container">
-       
-
-        <br />
-        <label className="label">Edit User's public key</label>
-        <br />
-        <input
-          value={userData.public_key}
-          // onChange={(e) => setPublic_key(e.target.value)}
-
-          type="text"
-          className="input"
-          onChange={this.handlePublicKeyChange}
-        />
-
-        <br />
-        <button
-          type="submit"
-          className="btn "
-          //@ts-ignore
-          onClick={this.handleUpdateUser}
-        >
-          Submit
-        </button>
-        <Link to={"/Users"}
-                            className="btn btn-sm btn-outline-secondary"
-                            > List of Users </Link>
-      </div>
-    );
-  }
+    </div>
+  );
 }
